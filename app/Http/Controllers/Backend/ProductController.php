7cb37328342de7ab1bd150441c788e5915060059
halimatussadia <sadia.kodeeo@gmail.com>
2021-11-13 17:15:21 +0600

    data insert

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -17,6 +18,20 @@ class ProductController extends Controller
     {
         return view('admin.layouts.product-create');
 
+    }
+
+    public function productStore(Request $request)
+    {
+        try {
+           Product::create([
+               'name' => $request->input('name'),
+               'price' => $request->input('price'),
+               'description' => $request->input('description'),
+           ]);
+           return redirect()->route('admin.products')->with('success', 'Product created successfully');
+        }catch (\Throwable $throwable){
+            return redirect()->back();
+        }
     }
 
 
